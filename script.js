363b3214dcc6029b45587fb49a0b513fb943c18f
imhazeltaylo <hazeltiffany.taylo.2342@gmail.com>
2023-04-23 09:48:40 +0800

    Add example for post method

--- script.js
+++ script.js
@@ -18,7 +18,8 @@ fetch("https://reqres.in/api/users/25")
 .then(data=>console.log(data))
 .catch(error => console.log("Error"));
 */
-
+ 
+/*
 fetch("https://reqres.in/api/users")
 .then(res=> {
     if(res.ok){
@@ -29,5 +30,23 @@ fetch("https://reqres.in/api/users")
     }
     res.json()
 })
+.then((data)=>console.log(data))
+.catch(error=>console.log("Error"));
+*/
+
+// GET, POST
+
+/* POST REQUEST */
+fetch("https://reqres.in/api/users",{
+    method: 'POST',
+    headers: {
+        'Content-type': 'application/json',
+    },
+    body: JSON.stringify({
+        name: 'New User 1',
+    }) 
+})
+.then(res=> {
+    return res.json()})
 .then((data)=>console.log(data))
 .catch(error=>console.log("Error"));
